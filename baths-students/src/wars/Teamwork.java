202f9c3ddb6b153bdc7cfef1f05e9bc365c0a14a
nm23aax <nm23aax@herts.ac.uk>
2025-04-06 13:23:42 +0100

    Fixed alphabetical order and array size

--- baths-students/src/wars/Teamwork.java
+++ baths-students/src/wars/Teamwork.java
@@ -9,7 +9,7 @@ package wars;
  */
 public class Teamwork
 {
-    private String[] details = new String[12];
+    private String[] details = new String[13];
     
     public Teamwork()
     {   // in each line replace the contents of the String 
@@ -19,28 +19,29 @@ public class Teamwork
         // before the surname of member 2...etc
         details[0] = "CWK1B CS56";
         
-        details[1] = "Organ";
-        details[2] = "Thomas";
-        details[3] = "22017656";
-
-        details[4] = "Ali";
-        details[5] = "Awab";
-        details[6] = "20049220";
-
+        details[1] = "Ali";
+        details[2] = "Awab";
+        details[3] = "20049220";
+        
+        details[4] = "Anyanwu";
+        details[5] = "Jordan";
+        details[6] = "21037141";
+        
         details[7] = "McEvoy";
         details[8] = "Nathan";
         details[9] = "20052541";
+        
+        details[10] = "Organ";
+        details[11] = "Thomas";
+        details[12] = "22017656";
 
-
-        details[10] = "Anyanwu";
-        details[11] = "Jordan";
-        details[12] = "21037141";
+       
 
 	
 	   // only if applicable
-        details[13] = "surname of member5";
-        details[14] = "first name of member5";
-        details[15] = "SRN of member5";
+//        details[13] = "surname of member5";
+//        details[14] = "first name of member5";
+//        details[15] = "SRN of member5";
 
 
     }
